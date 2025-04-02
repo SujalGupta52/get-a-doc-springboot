c9ab17bb9709c09e39190c485d560d88ad45f43d
SujalGupta52 <67575635+SujalGupta52@users.noreply.github.com>
2025-04-02 22:09:53 +0530

    Initial configuration

---
+++ src/main/java/com/webapp/getadoc/GetADocApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.getadoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetADocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetADocApplication.class, args);
+	}
+
+}
